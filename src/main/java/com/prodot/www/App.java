b2fb5e2bbb05108bfb058a39cd4d2824b69c8c74
root <root@ip-172-31-2-137.us-east-2.compute.internal>
2022-02-18 09:40:47 +0000

    friday file

---
+++ src/main/java/com/prodot/www/App.java
@@ -0,0 +1,13 @@
+package com.prodot.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       System.out.println( "Hello World!tODAY is friday" );
+    }
+}
